b8d2ef5b0b09e71a09cc3bab9c6af3a1f2fe4642
Daniel Pak <danpak94@gmail.com>
2024-03-13 22:33:20 -0400

    removing duplicate data setup methods

--- SlicerDeepCardiac/RoiPred/RoiPredLib/RoiPredLib.py
+++ SlicerDeepCardiac/RoiPred/RoiPredLib/RoiPredLib.py
@@ -29,42 +29,6 @@ except:
     pass
 import vtk
 
-curr_file_dir_path = os.path.dirname(os.path.realpath(__file__))
-dcvm_parent_dir = os.path.abspath(os.path.join(curr_file_dir_path, '../../..'))
-# dcvm_parent_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..'))
-temp_data_dir = os.path.abspath(os.path.join(dcvm_parent_dir, '../dcvm_data_files'))
-
-def relocate_data():
-    downloaded_exps_dir = os.path.join(temp_data_dir, 'experiments')
-    downloaded_template_dir = os.path.join(temp_data_dir, 'template_for_deform')
-    
-    dcvm_exps_dir = os.path.join(dcvm_parent_dir, 'experiments')
-    dcvm_template_dir = os.path.join(dcvm_parent_dir, 'template_for_deform')
-
-    move_matching_files(downloaded_exps_dir, dcvm_exps_dir)
-    move_matching_files(downloaded_template_dir, dcvm_template_dir)
-    
-    if os.path.isdir(temp_data_dir):
-        shutil.rmtree(temp_data_dir, ignore_errors=True)
-        print('Deleted: {}'.format(temp_data_dir))
-        print(' ')
-
-def move_matching_files(dirA, dirB):
-    for root, dirs, files in os.walk(dirA):
-        for dir_name in dirs:
-            source_dir = os.path.join(root, dir_name)
-            dest_dir = os.path.join(dirB, dir_name)
-            os.makedirs(dest_dir, exist_ok=True)
-            # if os.path.exists(dest_dir):
-            for file_name in os.listdir(source_dir):
-                source_file = os.path.join(source_dir, file_name)
-                dest_file = os.path.join(dest_dir, file_name)
-                if os.path.isfile(source_file):
-                    shutil.move(source_file, dest_file)
-                    print('src: {}'.format(source_file))
-                    print('dst: {}'.format(dest_file))
-                    print(' ')
-
 def img_from_volumeNode(volumeNode):
     img = slicer.util.arrayFromVolume(volumeNode) # RAS
     img = img.transpose(2,1,0) # b/c vtk/Slicer flips when getting array from volume
